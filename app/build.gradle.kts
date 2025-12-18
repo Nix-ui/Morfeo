f886999db41dd4e2ad19b2a8d3ee9ad065614326
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-12-18 17:13:58 -0400

    feat: Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ucb.morfeo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 15
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
